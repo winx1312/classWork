b7353dc77acff5c5cccd11e2a67163f1452608e1
winx1312 <winx1312@gmail.com>
2020-01-06 12:03:49 +0300

    new

--- WebP/src/arrow.js
+++ WebP/src/arrow.js
@@ -3,11 +3,9 @@ arrow.classList.add("arrow")
 arrow.addEventListener("click", function() {
     window.scrollTo(pageYOffset, 0);
 });
-// window.addEventListener('scroll', function(event) {
-
-//     if (pageYOffset > 300) {
-//         console.log(pageYOffset)
-//         arrow.style.display = "none"
-//     }
-// });
+window.addEventListener('scroll', function(event) {
+    if (pageYOffset > document.documentElement.clientHeight) {
+        arrow.classList.add('arrow-r')
+    }
+});
 document.body.append(arrow)
--- WebP/src/helmet.js
+++ WebP/src/helmet.js
@@ -1,12 +1,12 @@
 const hel = document.querySelector(".const")
 window.addEventListener('scroll', function(event) {
-    if (pageYOffset > 1500) {
-        hel.classList.add("visable")
-    }
-})
-document.querySelectorAll(".style__opt-wrapper").forEach(i => {
-i.classList.add("hide")
-i.addEventListener("click", function())
-})
+        if (pageYOffset > 1000) {
+            hel.classList.add("visable")
+        }
+    })
+    // document.querySelectorAll(".style__opt-wrapper").forEach(i => {
+    // i.classList.add("hide")
+    // i.addEventListener("click", function()){
 
-})
+// }
+// })
